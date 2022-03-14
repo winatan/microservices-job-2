62902164bf7dfe95f2f39a6bf715f51ee4a01a77
Jaswin <winatan@msn.com>
2022-03-14 12:23:53 +0800

    updated ore

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import requests
 import sqlite3
 from flask_openapi3 import Info, Tag
 from flask_openapi3 import OpenAPI
-
+from flask_cors import CORS
 
 info = Info(title='Taxi booking API', version='1.0.0')
 app = OpenAPI(__name__, info=info)
+CORS(app)
 
 taxi_tag = Tag(name='Taxi Booking', description='Booking Taxi services')
 
@@ -36,9 +37,9 @@ def add_booking(query: BookQuery):
     conn.commit()
     return {
         "message": "Booking made and committed to database",
-        "Booking": [
+        "Booking": 
                 {"Booking_id":query.booking_id, "Date": query.date, "time": query.time, "Pick up point": query.pick_up_point, "Destination" :query.destination,
-                "Current_Location_Latitude" : long, "Current_Location_Longitude": lat}]
+                "Current_Location_Latitude" : long, "Current_Location_Longitude": lat}
     }
 
 @app.get('/view_booking/<int:bid>', tags=[taxi_tag])
@@ -49,7 +50,9 @@ def view_booking(path: BookingPath):
         sqlite_query = """SELECT * FROM bookings WHERE booking_id = ?"""
         cursor.execute(sqlite_query, (path.bid,))
         results = cursor.fetchall()
-        return{'Requested Booking by booking id':results}
+        return{
+            'message:' : 'Returned Requested Booking by booking id',
+            'booking':results}
 
 @app.get('/view_all_bookings', tags=[taxi_tag])
 def view_all_bookings():
@@ -57,7 +60,8 @@ def view_all_bookings():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM bookings")
     results = cursor.fetchall()
-    return {'Bookings in database':results}
+    return {'bookings':results,
+        'message:' : 'Returned all Bookings'}
 
 
 @app.post('/update_booking/', tags=[taxi_tag])
